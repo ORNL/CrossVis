3bb5deced8b7f357f1cc67832f9947d1a0fac294
Halsey, William H <halseywh@ornl.gov>
2016-05-26 14:13:30 -0400

    deleted defunct classes

--- src/main/java/gov/ornl/csed/cda/Talon/SegmentedTimeSeriesPanelListener.java
+++
@@ -1,8 +0,0 @@
-package gov.ornl.csed.cda.Talon;
-
-/**
- * Created by whw on 4/29/16.
- */
-public interface SegmentedTimeSeriesPanelListener {
-    public void segmentTimeSeriesPanelSegmentClicked(SegmentedTimeSeriesPanel panel, Double segmentValue);
-}
