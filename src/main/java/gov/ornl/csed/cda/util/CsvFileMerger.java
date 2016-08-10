87c8eea19549b427f2d659ad04d2aa5e32d859be
Halsey, William H <halseywh@ornl.gov>
2016-08-10 08:12:52 -0400

    created new combiner class

---
+++ src/main/java/gov/ornl/csed/cda/util/CsvFileMerger.java
@@ -0,0 +1,24 @@
+package gov.ornl.csed.cda.util;/*
+ *
+ *  Class:  [CLASS NAME]
+ *
+ *      Author:     whw
+ *
+ *      Created:    10 Aug 2016
+ *
+ *      Purpose:    [A description of why this class exists.  For what
+ *                  reason was it written?  Which jobs does it perform?]
+ *
+ *
+ *  Inherits From:  [PARENT CLASS]
+ *
+ *  Interfaces:     [INTERFACES USED]
+ *
+ */
+
+public class CsvFileMerger {
+
+    public static void main(String[] args) {
+        return;
+    }
+}
