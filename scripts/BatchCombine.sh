080b404349beeb490e5e39d1d14c55dbb01f6ad8
William Halsey <whw@ornl.gov>
2016-10-12 09:47:34 -0400

    fixed names of batch parser and combiner files

--- scripts/BatchCombine.sh
+++ scripts/BatchCombine.sh
@@ -1,24 +1,24 @@
-# BatchParse.sh written by William Halsey
+# BatchCombine.sh written by William Halsey
 # whw@ornl.gov
 #
-# created: 15 Sept. 2016
+# created: 20 Sept. 2016
 #
 # Description: 
-# Script to batch parse PLG files to CSV files
-# Employs the PlgToCsvParser Java Class
+# Script to batch combine PLG and pore data
+# Employs the CsvFileMerger Java Class
 
 # PARAMETERS FOR THIS SCRIPT
-# $1 - parser type
-# $2 - PLG directory to batch process
+# $1 - appendee file directory
+# $2 - appender file directory
 # $3 - CSV directory for output
-# $4 - template file name
-# $5 - sample duration time
+# $4 - appendee key column
+# $5 - appender key column
 
-PARSER_TYPE=$1
-PLG_DIR=$2
+APPENDEE_DIR=$1
+APPENDER_DIR=$2
 CSV_DIR=$3
-TEMPLATE=$4
-DURATION=$5
+APPENDEE_KEY=$4
+APPENDER_KEY=$5
 
 # create the csv directory
 # need to iterate over all of the PLG files in a directory
@@ -42,7 +42,7 @@ cd $PLG_DIR
 for F in *
 do
 
-echo "Parsing $F ..."
+echo "Processing $F ..."
 java -Xmx8g -cp $HOME/../target/falcon-0.3.1-jar-with-dependencies.jar gov.ornl.csed.cda.util.PlgToCsvParser $PARSER_TYPE "$PLG_DIR/$F" "$CSV_DIR/$F.csv" "$TEMPLATE" $DURATION
 
 done
